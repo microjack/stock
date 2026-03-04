b46f91fab866064d13c039c0a763486983254811
wangjie@soyoung.com <wangjie@soyoung.com>
2026-03-04 11:38:43 +0800

    update

--- restart.sh
+++ restart.sh
@@ -4,4 +4,7 @@
 ps aux | grep "/Users/wangjie/pan/monitor.py" | grep -v grep | awk '{print $2}' | xargs kill -9
 
 # 启动
-nohup /usr/local/bin/python /Users/wangjie/pan/monitor.py > /dev/null 2>&1 &
+
+[ $(date +%H) -ge 15 ] || nohup /usr/local/bin/python /Users/wangjie/pan/monitor.py > /dev/null 2>&1 &
+
+exit
